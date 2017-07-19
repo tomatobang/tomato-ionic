e0305d549f9be892f08bed679a612582541dc48d
kobepeng <pengkobe123@gmail.com>
2017-07-19 19:38:39 +0800

    ready to test in android

--- src/pages/test/test.ts
+++ src/pages/test/test.ts
@@ -1,66 +1,227 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { Media, MediaObject } from "@ionic-native/media";
+import { ElementRef } from "@angular/core";
 
 import { NavController, IonicPage } from "ionic-angular";
 
+import { Platform } from "ionic-angular";
+import { Gesture } from "ionic-angular/gestures/gesture";
+
+declare var cordova;
+
 @IonicPage()
 @Component({
-  selector: "page-home",
-  templateUrl: "home.html"
+  selector: "page-test",
+  templateUrl: "test.html",
+  providers:[Media]
 })
-export class TestPage {
-  toUser: Object;
+export class TestPage implements OnInit, OnDestroy {
+  el: HTMLElement;
+  pressGesture: Gesture;
+  isStartRecord = false;
+  recordWait = false;
+  isStartedVoice = false;
+  mediaRec: MediaObject;
+  src = "";
+  path = "";
 
-  constructor(public navCtrl: NavController, private media: Media) {
-    this.toUser = {
-      toUserId: "210000198410281948",
-      toUserName: "Hancock"
-    };
+  constructor(
+    public navCtrl: NavController,
+    private media: Media,
+    public platform: Platform,
+    private elRef: ElementRef
+  ) {
+    if (this.platform.is("IOS")) {
+      this.path = cordova ? cordova.file.documentsDirectory : "";
+      this.src = "cordovaIMVoice.wav";
+    } else {
+      this.path = cordova
+        ? cordova.file.externalApplicationStorageDirectory
+        : "";
+      this.src = "cordovaIMVoice.amr";
+    }
+    this.el = elRef.nativeElement;
   }
 
-  recordVoice() {
-    const file: MediaObject = this.media.create("file.mp3");
+  ngOnInit() {
+    this.pressGesture = new Gesture(this.el);
+    this.pressGesture.listen();
+    // 长按录音
+    this.pressGesture.on("press", e => {
+      this.onVoiceHold();
+    });
+    // 释放则播放
+    this.pressGesture.on("pressup", e => {
+      this.onVoiceRelease();
+    });
+  }
 
+  ngOnDestroy() {
+    this.pressGesture.destroy();
+  }
+
+  onVoiceHold() {
+    this.isStartRecord = true;
+    this.recordWait = false;
+    try {
+      //实例化录音类
+      this.startRec();
+      //开始录音
+      this.mediaRec.startRecord();
+      //已经开始
+      this.isStartedVoice = true;
+      return false;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  startRec() {
+    if (this.mediaRec) {
+      this.mediaRec.release();
+    }
+
+    //实例化录音类
+    this.mediaRec = this.media.create(this.getNewMediaURL(this.src));
     // to listen to plugin events:
 
-    file.onStatusUpdate.subscribe(status => console.log(status)); // fires when file status changes
+    this.mediaRec.onStatusUpdate.subscribe(status => console.log(status)); // fires when file status changes
+    // 录音执行函数
+    this.mediaRec.onSuccess.subscribe(() =>
+      console.log("Action is successful")
+    );
+    // 录音失败执行函数
+    this.mediaRec.onError.subscribe(error => console.log("Error!", error));
 
-    file.onSuccess.subscribe(() => console.log("Action is successful"));
+    // 模拟声音大小变化
+    var voicechange = setInterval(function() {
+      if (!this.recordWait) {
+        var i = Math.round(Math.random() * 9);
+        this.voiceImg.url = "asset/img/chat/voice/recog00" + i + ".png";
+      } else {
+        voicechange = undefined;
+      }
+    }, 400);
+  }
+  play(voiFile, type) {
+    if (this.mediaRec) {
+      this.mediaRec.stop();
+      this.mediaRec.release();
+    }
+    // this.elRef.nativeElement.querySelector('div');
+    // var target = angular.element(event.target).find("i");
+    // if (type == "you") {
+    //   target.addClass("web_wechat_voice_gray_playing");
+    // } else {
+    //   target.addClass("web_wechat_voice_playing");
+    // }
+    // if (this.platform.is("IOS")) {
+    //   voiFile = voiFile.replace("file://", "");
+    // }
+    // this.mediaRec = this.media.create(voiFile);
 
-    file.onError.subscribe(error => console.log("Error!", error));
+    // // 录音执行函数
+    // this.mediaRec.onSuccess.subscribe(() => {
+    //   //消息属于自己还是好友，前台会传you or me
+    //   if (type == "you") {
+    //     target.removeClass("web_wechat_voice_gray_playing");
+    //   } else {
+    //     target.removeClass("web_wechat_voice_playing");
+    //   }
+    //   console.log("play():Audio Success");
+    // });
+    // // 录音失败执行函数
+    // this.mediaRec.onError.subscribe(error => {
+    //   if (type == "you") {
+    //     target.removeClass("web_wechat_voice_gray_playing");
+    //   } else {
+    //     target.removeClass("web_wechat_voice_playing");
+    //   }
+    //   console.log("play():Audio Error: ", error);
+    // });
 
-    // play the file
-    file.play();
+    // //开始播放录音
+    // this.mediaRec.play();
+    // return false;
+  }
 
-    // pause the file
-    file.pause();
+  onVoiceRelease() {
+    //如果没有开始直接返回
+    if (!this.isStartedVoice) {
+      return;
+    }
+    //还原标识
+    this.isStartedVoice = false;
+    this.recordWait = true;
+    setTimeout(function() {
+      this.isStartRecord = false;
+    }, 1000);
+    if (this.mediaRec) {
+      this.mediaRec.stopRecord();
+      this.mediaRec.release();
+    }
+    //实例化录音类, src:需要播放的录音的路径
+    this.mediaRec = this.media.create(this.getMediaURL(this.src));
+    // 录音执行函数
+    this.mediaRec.onSuccess.subscribe(() =>
+      console.log("touchend():Audio Success")
+    );
+    // 录音失败执行函数
+    this.mediaRec.onError.subscribe(error =>
+      console.log("touchend():Audio Error!", error)
+    );
+    this.mediaRec.play();
+    // this.mediaRec.stop();
+    //在html中显示当前状态
+    var counter = 0;
+    // var timerDur = setInterval(function() {
+    //   counter = counter + 100;
+    //   if (counter > 2000) {
+    //     clearInterval(timerDur);
+    //   }
+    //   var dur = this.mediaRec.getDuration();
+    //   if (dur > 0) {
+    //     clearInterval(timerDur);
+    //     // alert('mediaRec.getDuration():' + dur);
+    //     // alert('mediaRec.src:' + mediaRec.src);
+    //     var tmpPath = this.mediaRec.src;
+    //     if (this.platform.is("ios")) {
+    //       tmpPath = this.path + this.src;
+    //     }
+    //     tmpPath = tmpPath.replace("file://", "");
+    //     // 发送语音消息
+    //     // RongyunUtil.sendVoiceMessage(
+    //     //   $stateParams.conversationType,
+    //     //   $stateParams.targetId,
+    //     //   tmpPath,
+    //     //   dur,
+    //     //   this.mediaRec,
+    //     //   function(ret) {
+    //     //     this.lstResult = "sendVoiceMessage:" + JSON.stringify(ret);
+    //     //     // TODO:消息此时未发送成功，可以加入样式标明；成功后更新样式
+    //     //     if (ret.status == "prepare") {
+    //     //       // ret.result.message在prepare阶段才能读取到
+    //     //       appendNewMsg(ret.result.message, true);
+    //     //     }
+    //     //     // TODO:后续加入发送成功后修改显示样式
+    //     //     if (ret.status == "success") {
+    //     //       viewScroll.scrollBottom(true);
+    //     //     }
+    //     //   }
+    //     // );
+    //   }
+    // }, 100);
+    return false;
+  }
 
-    // get current playback position
-    file.getCurrentPosition().then(position => {
-      console.log(position);
-    });
+  getNewMediaURL(s) {
+    if (this.platform.is("android")) return this.path + s;
+    return "documents://" + s;
+  }
 
-    // get file duration
-    let duration = file.getDuration();
-    console.log(duration);
-
-    // skip to 10 seconds (expects int value in ms)
-    file.seekTo(10000);
-
-    // stop playing the file
-    file.stop();
-
-    // release the native audio resource
-    // Platform Quirks:
-    // iOS simply create a new instance and the old one will be overwritten
-    // Android you must call release() to destroy instances of media when you are done
-    file.release();
-
-    // Recording to a file
-    const file2: MediaObject = this.media.create("path/to/file.mp3");
-
-    file2.startRecord();
-
-    file2.stopRecord();
+  getMediaURL(s) {
+    if (this.platform.is("android")) {
+      return this.path + s;
+    }
+    return (this.path + s).replace("file://", "");
   }
 }
