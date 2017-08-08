f638de79605d78bb474bb846a1310bd6f6814bcf
kobepeng <pengkobe123@gmail.com>
2017-08-08 20:11:13 +0800

    fix ibeacon service

--- src/_util/ibeacon.service.ts
+++ src/_util/ibeacon.service.ts
@@ -18,17 +18,64 @@ export class IBeaconService {
 	) {}
 
 	init() {
-		this.ibeacon.requestAlwaysAuthorization();
 		// create a new delegate and register it with the native layer
 		let delegate = this.ibeacon.Delegate();
 
+		// 测试 beacon 设备
+		let beaconRegion = this.ibeacon.BeaconRegion(
+			"estimote",
+			"b9407f30-f5f8-466e-aff9-25556b57fe6d",
+			null,
+			null,
+			true
+		);
+
+		this.ibeacon.isBluetoothEnabled().then(value => {
+			if (!value) {
+				alert("蓝牙未打开");
+			}
+		});
+
+		this.ibeacon.requestAlwaysAuthorization().then(d => {
+			this.ibeacon
+				.startMonitoringForRegion(beaconRegion)
+				.then(
+					() =>
+						console.log(
+							"Native layer recieved the request to monitoring"
+						),
+					error =>
+						console.error(
+							"Native layer failed to begin monitoring: ",
+							error
+						)
+				);
+		});
+
 		// Subscribe to some of the delegate's event handlers
-		delegate
-			.didRangeBeaconsInRegion()
-			.subscribe(
-				data => console.log("didRangeBeaconsInRegion: ", data),
-				error => console.error()
-			);
+		delegate.didRangeBeaconsInRegion().subscribe(
+			data => {
+				console.log(data.beacons);
+				for (var i = 0; i < data.beacons.length; i++) {
+					if (
+						data.beacons[i].major == 0 &&
+						data.beacons[i].minor == 1
+					) {
+						// alert("总部")
+					} else if (
+						data.beacons[i].major == 1 &&
+						data.beacons[i].minor == 1
+					) {
+						// alert("园区")
+					}
+				}
+				// 停止扫描
+				this.ibeacon
+					.stopRangingBeaconsInRegion(beaconRegion)
+					.then(d => {});
+			},
+			error => console.error()
+		);
 		delegate
 			.didStartMonitoringForRegion()
 			.subscribe(
@@ -38,24 +85,5 @@ export class IBeaconService {
 		delegate.didEnterRegion().subscribe(data => {
 			console.log("didEnterRegion: ", data);
 		});
-
-		let beaconRegion = this.ibeacon.BeaconRegion(
-			"deskBeacon",
-			"b9407f30-f5f8466e-aff92555-6b57fe6d"
-		);
-
-		this.ibeacon
-			.startMonitoringForRegion(beaconRegion)
-			.then(
-				() =>
-					console.log(
-						"Native layer recieved the request to monitoring"
-					),
-				error =>
-					console.error(
-						"Native layer failed to begin monitoring: ",
-						error
-					)
-			);
 	}
 }
