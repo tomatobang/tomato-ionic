fcbb4b825a1d0aed46646feb3c0fc58a0dd2ebf2
kobepeng <pengkobe123@gmail.com>
2017-08-21 20:56:56 +0800

    add TomatoIOService

--- src/_util/socket.io.service.ts
+++ src/_util/socket.io.service.ts
@@ -23,28 +23,94 @@ export class SocketIOService {
 		this.socket.emit("request_other_pos", to_userid);
 	}
 
-	postMyPos(to_userid, pos){
-		this.socket.emit("post_my_pos", to_userid,pos);
+	postMyPos(to_userid, pos) {
+		this.socket.emit("post_my_pos", to_userid, pos);
 	}
 
 	/**
 	 * 被要求提交位置
 	 */
-	PleasePostPos(){
+	PleasePostPos() {
 		return this.socket.fromEvent<any>("please_post_pos").map(data => data);
 	}
 
 	/**
 	 * 收到位置信息
 	 */
-    receiveOtherPos() {
-		return this.socket.fromEvent<any>("received_other_pos").map(data => data);
-    }
-	
+	receiveOtherPos() {
+		return this.socket
+			.fromEvent<any>("received_other_pos")
+			.map(data => data);
+	}
+
 	/**
 	 * 位置收发错误
 	 */
-    sendPos_error(){
-        return this.socket.fromEvent<any>("location_error").map(data => data);
-    }
+	sendPos_error() {
+		return this.socket.fromEvent<any>("location_error").map(data => data);
+	}
+}
+
+
+
+import {Tomato} from "../providers/model_api/tomato"
+
+@Injectable()
+export class TomatoIOService {
+	constructor(private socket: Socket) {}
+
+
+	/**
+	 * 第一次，用于加载当前tomato
+	 */
+	load_tomato(userid:String){
+		this.socket.emit("load_tomato", {userid,endname:"ionic"},);
+	}
+
+	load_tomato_succeed(){
+		return this.socket.fromEvent<any>("load_tomato_succeed").map(data => data);
+	}
+
+	/**
+	 * 其它终端中断番茄钟
+	 */
+	other_end_break_tomato() {
+		return this.socket.fromEvent<any>("other_end_break_tomato").map(data => data);
+	}
+	/**
+	 * 中断番茄钟
+	 */
+	break_tomato(userid:String,tomato:Tomato) {
+		this.socket.emit("break_tomato",{
+			userid,tomato
+		} );
+	}
+
+	/**
+	 * 中断番茄钟
+	 */
+	break_tomato_succeed() {
+		this.socket.fromEvent<any>("break_tomato_succeed").map(data => data);
+	}
+
+	/**
+	 * 其它终端开启番茄钟
+	 */
+	other_end_start_tomato() {
+		return this.socket.fromEvent<any>("other_end_start_tomato").map(data => data);
+	}
+
+	/**
+	 * 开启番茄钟
+	 */
+	start_tomato(userid:String,tomato:Tomato) {
+		this.socket.emit("start_tomato", {userid,tomato});
+	}
+
+	/**
+	 * 开启番茄钟
+	 */
+	start_tomato_succeed(tomato:Tomato) {
+		this.socket.emit("start_tomato_succeed", tomato);
+	}
 }
