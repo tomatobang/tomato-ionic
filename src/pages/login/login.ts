40fff7be13a00b9020c50a261fb06c8503ac5b5c
kobepeng <pengkobe123@gmail.com>
2017-08-29 21:08:47 +0800

    make login work

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -6,18 +6,17 @@
  */
 
 import { Component } from "@angular/core";
-import { NavController,IonicPage } from 'ionic-angular';
+import { NavController,IonicPage,ToastController} from 'ionic-angular';
 import { OnlineUserService, User } from "../../providers/data.service";
 import { GlobalService } from "../../providers/global.service";
-
 import { RebirthHttpProvider } from "rebirth-http";
 
 @IonicPage()
 @Component({
 	selector: "login",
 	providers: [OnlineUserService, GlobalService],
-	templateUrl: "login.html"
-	//styleUrls: ["./login.component.css"],
+	templateUrl: "login.html",
+	//styleUrls: ["./login.scss"],
 	
 })
 export class LoginPage {
@@ -31,7 +30,8 @@ export class LoginPage {
 		public service: OnlineUserService,
 		public globalservice: GlobalService,
 		public rebirthProvider: RebirthHttpProvider,
-		public navCtrl:NavController
+		public navCtrl:NavController,
+		private toastCtrl: ToastController
 	) {}
 
 	ngOnInit() {
@@ -40,27 +40,27 @@ export class LoginPage {
 
 	public doLogin(): void {
 		console.log("doLogin", this.user);
-		this.navCtrl.push("TabsPage",{},()=>{
-
-		})
-		// this.service.login(this.user).subscribe(data => {
-		// 	let retOBJ = JSON.parse(data._body);
-		// 	let status = retOBJ.status;
-		// 	let token = "";
-		// 	if (status == "success") {
-		// 		token = retOBJ.token;
-		// 	} else {
-		// 		this.error = "登陆出错！";
-		// 		return;
-		// 	}
-		// 	console.log(data);
-		// 	// 这里需要保存 token
-		// 	this.globalservice.token = token;
-		// 	this.globalservice.userinfo = JSON.stringify(this.user);
-		// 	this.rebirthProvider.headers({ Authorization: token });
-		// 	// 页面跳转
-		// 	//this.router.navigate(['/dash'] , { replaceUrl: true});
-		// });
+		
+		this.service.login(this.user).subscribe(data => {
+			let retOBJ = JSON.parse(data._body);
+			let status = retOBJ.status;
+			let token = "";
+			if (status == "success") {
+				token = retOBJ.token;
+			} else {
+				this.error = "登陆出错！";
+				return;
+			}
+			console.log(data);
+			// 这里需要保存 token
+			this.globalservice.token = token;
+			this.globalservice.userinfo = JSON.stringify(this.user);
+			this.rebirthProvider.headers({ Authorization: token });
+			this.navCtrl.push("TabsPage",{},()=>{
+			});
+			// 页面跳转
+			//this.router.navigate(['/dash'] , { replaceUrl: true});
+		});
 	}
 
 	public doLogout(): void {
@@ -71,4 +71,18 @@ export class LoginPage {
 		// 注册
 		//this.router.navigate(['/user/register']);
 	}
+
+	presentToast(msg) {
+		let toast = this.toastCtrl.create({
+		  message: msg,
+		  duration: 3000,
+		  position: 'top'
+		});
+	  
+		toast.onDidDismiss(() => {
+		  console.log('Dismissed toast');
+		});
+	  
+		toast.present();
+	  }
 }
