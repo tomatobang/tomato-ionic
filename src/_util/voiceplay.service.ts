3b24706b85bb8614ce8485d28d95ff750b908958
kobepeng <pengkobe123@gmail.com>
2017-09-04 19:13:21 +0800

    finish download&play function of voice play

--- src/_util/voiceplay.service.ts
+++ src/_util/voiceplay.service.ts
@@ -6,16 +6,105 @@ import { Platform } from "ionic-angular";
 import { Events } from "ionic-angular";
 import { GlobalService } from "../providers/global.service";
 
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+import { Media, MediaObject } from "@ionic-native/media";
 declare var window;
 
 @Injectable()
 export class VoicePlayService {
+	mediaRec: MediaObject;
+
 	constructor(
-		public plf: Platform,
+		public platform: Platform,
+		private media: Media,
 		public _global: GlobalService,
-	) {}
+		private transfer: FileTransfer, private file: File
+	) { }
 
-	init() {
+	init() {}
 
+	downloadVoiceFile(filename) {
+		var targetPath = this.getBasePath() + 'voices/' + filename;
+		const fileTransfer: FileTransferObject = this.transfer.create();
+		// Download a file:
+		let options = {};
+		let trustHosts = true;
+		fileTransfer.download(this._global.serverAddress+"/download/voicefile/"+filename, targetPath, 
+		trustHosts,
+		 options).then(result => {
+			 // 下载完成后，直接播放
+			 this.play(targetPath);
+		}).catch(err => {
+			console.log("下载音频文件出错",err);
+		});
+
+		fileTransfer.onProgress((evt:ProgressEvent)=>{
+			let progress = window.parseInt(evt.loaded /evt.total * 100);
+			// 在这里可以更新下载进度
+		})
+	}
+
+	getBasePath() {
+		var basePath;
+		if (this.platform.is("ios")) {
+			basePath = window.cordova.file.documentsDirectory + "TomatoBang/";
+		} else {
+			basePath = window.cordova.file.externalApplicationStorageDirectory;
+		}
+		return basePath;
+	}
+
+
+	getFileName(url) {
+        var arr = url.split('/');
+        var fileName = arr[arr.length - 1];
+        return fileName;
+      }
+
+	//检查路径中是否存在这个文件，并相应改变state
+	checkFile(dataList) {
+		var targetPath = this.getBasePath() + 'voices/';
+		for (var i = 0; i < dataList.length; i++) {
+			targetPath = this.getBasePath() + 'voices/';
+			let index = i;
+			// 0 代表未下载，3 代表下载完成
+			this.file.checkFile(targetPath, this.getFileName(dataList[index].FilePath)).then(
+				function (success) {
+					dataList[index].state = 3;
+				}, function (error) {
+					dataList[index].state = 0;
+				})
+		}
+	}
+
+	/**
+	 * 播放
+	 * @param voiFile 文件路径
+	 */
+	play(voiFile) {
+		if (this.mediaRec) {
+			this.mediaRec.stop();
+			this.mediaRec.release();
+		}
+		if (!voiFile) {
+			return;
+		}
+		if (this.platform.is("IOS")) {
+			voiFile = voiFile.replace("file://", "");
+		}
+		this.mediaRec = this.media.create(voiFile);
+
+		this.mediaRec.onSuccess.subscribe(() => {
+			// 播放完成
+			console.log("play():Audio Success");
+		});
+		this.mediaRec.onError.subscribe(error => {
+			// 播放失败
+			console.log("play():Audio Error: ", error);
+		});
+
+		//开始播放录音
+		this.mediaRec.play();
 	}
 }
