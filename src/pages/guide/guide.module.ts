3b36c67bd69ed4684dd52895ef05e8717594dc0f
kobepeng <pengkobe123@gmail.com>
2017-09-11 16:52:50 +0800

    add guide-page refer to ionic-conference-app

---
+++ src/pages/guide/guide.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { GuidePage } from './guide';
+
+@NgModule({
+    declarations: [
+        GuidePage,
+    ],
+    imports: [
+        IonicPageModule.forChild(GuidePage),
+    ],
+    exports: [
+        GuidePage
+    ]
+})
+export class GuidePageModule {}
