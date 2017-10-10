0e1b0791041dbe94fa35e33993d3c2bf587c18ce
pengkobe <yipeng.info@gmail.com>
2017-10-10 22:06:16 +0800

    rm unused dependency and fix rongyunUtil refer err

--- src/pages/test/rongyun/rongyun.module.ts
+++ src/pages/test/rongyun/rongyun.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { RongyunPage } from "./rongyun";
 import { IonicPageModule } from "ionic-angular";
 import { RongYunService } from "../../../_util/rongyun.service";
-import { RongyunUtil } from "../../../_util/RongyunUtil";
+import { RongyunUtil } from "../../../_util/rongyun.util";
 
 
 @NgModule({
