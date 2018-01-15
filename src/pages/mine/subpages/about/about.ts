ddd40a788dae9fc2d742e8c29f37117e05c538a6
kobepeng <pengkobe123@gmail.com>
2018-01-15 13:42:57 +0800

    reafactor: friends page style

--- src/pages/mine/subpages/about/about.ts
+++ src/pages/mine/subpages/about/about.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit, ViewChild } from '@angular/core';
 
-import { IonicPage } from "ionic-angular";
+import { IonicPage } from 'ionic-angular';
 // import { GlobalService } from "../../../../providers/global.service";
 
 declare var window;
 @IonicPage()
 @Component({
-  selector: "page-about",
-  templateUrl: "about.html"
+  selector: 'page-about',
+  templateUrl: 'about.html',
 })
 export class AboutPage implements OnInit {
+  appversion: any;
   constructor() //public globalservice: GlobalService
   {
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (window.cordova) {
+      window.cordova.getAppVersion.getVersionNumber().then(version => {
+        this.appversion = version;
+      });
+    }
+  }
 }
