5a3403841efd9d72e32e8f5a5ecca0b3e2ad1885
kobepeng <pengkobe123@gmail.com>
2018-01-15 21:07:20 +0800

    feat: add qr-scanner & fix few cordova plugins

--- src/pages/friend/friend.module.ts
+++ src/pages/friend/friend.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from "@angular/core";
-import { FriendPage } from "./friend";
-import { IonicPageModule } from "ionic-angular";
+import { NgModule } from '@angular/core';
+import { FriendPage } from './friend';
+import { IonicPageModule } from 'ionic-angular';
+import { QRScanner } from '@ionic-native/qr-scanner';
 
 @NgModule({
   declarations: [FriendPage],
-  imports: [IonicPageModule.forChild(FriendPage)]
+  imports: [IonicPageModule.forChild(FriendPage)],
+  providers: [QRScanner],
 })
 export class FriendPageModule {}
--- src/pages/friend/friend.ts
+++ src/pages/friend/friend.ts
@@ -1,19 +1,40 @@
-import { Component } from "@angular/core";
-
-import { NavController, IonicPage } from "ionic-angular";
+import { Component } from '@angular/core';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+import { NavController, IonicPage } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: "page-friend",
-  templateUrl: "friend.html"
+  selector: 'page-friend',
+  templateUrl: 'friend.html',
 })
 export class FriendPage {
-  showType = "hot";
+  showType = 'hot';
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, private qrScanner: QRScanner) {}
 
   toMessagePage() {
-    console.log("setting!");
-    this.navCtrl.push("MessagePage", {}, {}, () => {});
+    this.navCtrl.push('MessagePage', {}, {}, () => {});
+  }
+
+  scanToAddFriend() {
+    console.log('scanToAddFriend!');
+    this.qrScanner
+      .prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+            console.log('Scanned something', text);
+            alert('您扫描到了以下内容:' + JSON.stringify(text));
+            this.qrScanner.hide(); 
+            scanSub.unsubscribe(); 
+          });
+          this.qrScanner.show();
+        } else if (status.denied) {
+          alert('您已拒绝授予 TomatoBang 相机权限！');
+        } else {
+          alert('您暂时拒绝了授予 TomatoBang 相机权限！');
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
   }
 }
