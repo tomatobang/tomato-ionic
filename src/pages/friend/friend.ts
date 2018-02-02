59c2c368396dac217c90180658c02d297bb4a24d
kobepeng <pengkobe123@gmail.com>
2018-02-02 10:57:09 +0800

    feat: build rank ui

--- src/pages/friend/friend.ts
+++ src/pages/friend/friend.ts
@@ -8,6 +8,56 @@ import { QRScannerComponent } from '../../components/qr-scanner/qr-scanner';
   templateUrl: 'friend.html',
 })
 export class FriendPage {
+  public rankList: {
+    title: string;
+    image: string;
+    tomatoNum: Number;
+    minute: Number;
+    rankText: string;
+    class: string;
+  }[] = [
+    {
+      title: '加油就有收获！',
+      tomatoNum: 9,
+      minute: 223,
+      image: 'assets/imgs/logo.png',
+      rankText: 'No. 1',
+      class: 'rank1',
+    },
+    {
+      title: '努力就能成功！',
+      tomatoNum: 8,
+      minute: 193,
+      image: 'assets/tomato-active.png',
+      rankText: 'No. 2',
+      class: 'rank2',
+    },
+    {
+      title: '天道酬勤，人道酬信！',
+      tomatoNum: 5,
+      minute: 123,
+      image: 'assets/tomato-active.png',
+      rankText: 'No. 3',
+      class: 'rank3',
+    },
+    {
+      title: '抓效率！',
+      image: 'assets/tomato-grey.png',
+      tomatoNum: 1,
+      minute: 23,
+      rankText: '',
+      class: '',
+    },
+    {
+      title: '我爱番茄帮！',
+      image: 'assets/tomato-grey.png',
+      tomatoNum: 1,
+      minute: 21,
+      rankText: '',
+      class: '',
+    },
+  ];
+
   showType = 'hot';
   ionApp: HTMLElement;
   @ViewChild('qrScanner') qrScanner: QRScannerComponent;
@@ -16,21 +66,21 @@ export class FriendPage {
 
   /**
    * 跳转至消息页
-  */
+   */
   toMessagePage() {
     this.navCtrl.push('MessagePage', {}, {}, () => {});
   }
 
   /**
    * 扫码加友
-  */
+   */
   scanToAddFriend() {
     this.qrScanner.open();
   }
 
   /**
    * 跳转至通讯录
-  */
+   */
   ToContactsPage() {
     this.navCtrl.push('ContactsPage', {}, {}, () => {});
   }
