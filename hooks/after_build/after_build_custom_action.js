f13838ac7eeb6a2ebf2d7328a5dd3ba3d2832178
kobepeng <pengkobe123@gmail.com>
2018-02-07 13:45:45 +0800

    feat: add hooks for test purpose

---
+++ hooks/after_build/after_build_custom_action.js
@@ -0,0 +1,24 @@
+module.exports = function (ctx) {
+    // make sure android platform is part of build
+    console.log('after_build_custom_action: start');
+    if (ctx.opts.platforms.indexOf('android') < 0) {
+        return;
+    }
+    var fs = ctx.requireCordovaModule('fs'),
+        path = ctx.requireCordovaModule('path'),
+        deferral = ctx.requireCordovaModule('q').defer();
+
+    var platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android');
+    var apkFileLocation = path.join(platformRoot, 'build/outputs/apk/android-debug.apk');
+
+    fs.stat(apkFileLocation, function (err, stats) {
+        if (err) {
+            deferral.reject('Operation failed');
+        } else {
+            console.log('Size of ' + apkFileLocation + ' is ' + stats.size + ' bytes');
+            deferral.resolve();
+        }
+    });
+
+    return deferral.promise;
+};
