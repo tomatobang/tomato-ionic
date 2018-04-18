a32cbd7dddc42b3968f093ca8232f8f1f4cf6087
kobepeng <pengkobe123@gmail.com>
2018-04-18 19:41:02 +0800

    feat: add 2048 mobile support

--- src/pages/game/twoZeroFourEight/twoZeroFourEight.ts
+++ src/pages/game/twoZeroFourEight/twoZeroFourEight.ts
@@ -1,7 +1,15 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import {
+  Component,
+  HostListener,
+  OnInit,
+  ViewChild,
+  ElementRef,
+} from '@angular/core';
 import { GameService } from './services/game.service';
 import { DIRECTIONS } from './enums/index';
-import { IonicPage } from 'ionic-angular';
+import { IonicPage, Events } from 'ionic-angular';
+import { Gesture } from 'ionic-angular/gestures/gesture';
+declare var Hammer: any;
 
 @Component({
   selector: 'TwoZeroFourEight',
@@ -9,7 +17,10 @@ import { IonicPage } from 'ionic-angular';
 })
 @IonicPage()
 export class TwoZeroFourEightPage implements OnInit {
-  constructor(public game: GameService) {}
+  swipeGesture: Gesture;
+  @ViewChild('gameContainer') gameContainer: ElementRef;
+
+  constructor(public game: GameService, public events: Events) {}
 
   public newGame(): void {
     this.game.newGame();
@@ -17,6 +28,36 @@ export class TwoZeroFourEightPage implements OnInit {
 
   ngOnInit() {
     this.newGame();
+  }
+
+  ionViewDidLoad() {
+    this.swipeGesture = new Gesture(this.gameContainer.nativeElement, {
+      recognizers: [[Hammer.Swipe, { direction: Hammer.DIRECTION_ALL }]],
+    });
+    this.swipeGesture.listen();
+    let key = '';
+    this.swipeGesture.on('swipeleft', e => {
+      console.log('swipeleft');
+      key = DIRECTIONS[37];
+      this.game.move(key);
+    });
+    this.swipeGesture.on('swiperight', e => {
+      console.log('swiperight');
+      key = DIRECTIONS[39];
+      this.game.move(key);
+    });
+
+    this.swipeGesture.on('swipeup', e => {
+      console.log('swipeup');
+      key = DIRECTIONS[38];
+      this.game.move(key);
+    });
+
+    this.swipeGesture.on('swipedown', e => {
+      console.log('swipedown');
+      key = DIRECTIONS[40];
+      this.game.move(key);
+    });
   }
 
   @HostListener('window:keydown', ['$event'])
