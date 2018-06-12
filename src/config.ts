fa6ef9db8284b6fe26475a02b96c6ceb08a2a9c2
kobepeng <pengkobe123@gmail.com>
2018-06-12 14:24:08 +0800

    chore: fix environment variable

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,8 @@
 import { ENV } from '@app/env';
 
-const serverUrl = 'http://47.100.16.77:7003/';
 console.log('ENV:', ENV.mode);
-// if (ENV.mode === 'prod') {
-//     serverUrl = 'http://47.100.16.77:7001/';
-// } else {
-//     serverUrl = 'http://192.168.3.223:7001/';
-// }
-export const baseUrl = serverUrl;
+console.log('ENV serverUrl:', ENV.serverUrl);
+export const baseUrl = ENV.serverUrl;
 
-export const chatSocketUrl = serverUrl + 'chat';
-export const tomatoSocketUrl = serverUrl + 'tomatobang';
+export const chatSocketUrl = ENV.serverUrl + 'chat';
+export const tomatoSocketUrl = ENV.serverUrl + 'tomatobang';
