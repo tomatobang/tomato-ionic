62b75dd5a030496f9df6aa5408dc4b96aabd891f
kobepeng <pengkobe123@gmail.com>
2019-02-28 12:44:31 +0800

    chore: clean unused code

--- src/app/pages/list/dynamic-form/readme.md
+++
@@ -1,3 +0,0 @@
-# notes
-
-the dynamic-form demo is reference from `https://toddmotto.com/angular-dynamic-components-forms`
--- src/app/pages/list/list.router.module.ts
+++ src/app/pages/list/list.router.module.ts
@@ -1,27 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-import { CalendarPage } from './calendar/calendar.page';
-import { EchartsPage } from './echarts/echarts';
-import { DynamicFormPage } from './dynamic-form/dynamic-form';
-import { ListPage } from './list.page';
+// import { ListPage } from './list.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: ListPage,
-  },
-  {
-    path: 'calendar',
-    component: CalendarPage,
-  },
-  {
-    path: 'echarts',
-    component: EchartsPage,
-  },
-  {
-    path: 'dynamicform',
-    component: DynamicFormPage,
+    loadChildren: './ngrxtodo/ngrxtodo.module#NgrxTodoPageModule',
   },
   {
     path: 'todo',
