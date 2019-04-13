4bef1d82531d864e3f6f9e862f4d93c14e9d435f
kobepeng <pengkobe123@gmail.com>
2019-04-13 10:14:15 +0800

    refactor: rm unused content in friend module(ranking&working)

--- src/app/pages/friend/friend.ts
+++ src/app/pages/friend/friend.ts
@@ -16,59 +16,7 @@ export class FriendPage implements OnInit {
   pullingIcon = false;
   isShowMenuCard = true;
 
-  public rankList: {
-    title: string;
-    image: string;
-    tomatoNum: Number;
-    minute: Number;
-    rankText: string;
-    class: string;
-  }[] = [
-      {
-        title: '加油就有收获！',
-        tomatoNum: 11,
-        minute: 223,
-        image: 'assets/imgs/logo.png',
-        rankText: '1st',
-        class: 'rank1',
-      },
-      {
-        title: '努力就能成功！',
-        tomatoNum: 8,
-        minute: 193,
-        image: 'assets/tomato-active.png',
-        rankText: '2nd',
-        class: 'rank2',
-      },
-      {
-        title: '天道酬勤，人道酬信！',
-        tomatoNum: 5,
-        minute: 123,
-        image: 'assets/tomato-active.png',
-        rankText: '3rd',
-        class: 'rank3',
-      },
-      {
-        title: '抓效率！',
-        image: 'assets/tomato-grey.png',
-        tomatoNum: 1,
-        minute: 23,
-        rankText: '',
-        class: '',
-      },
-      {
-        title: '我爱番茄帮！',
-        image: 'assets/tomato-grey.png',
-        tomatoNum: 1,
-        minute: 21,
-        rankText: '',
-        class: '',
-      },
-    ];
-
-  showType = 'hot';
   isPullToShow = false;
-  ionApp: HTMLElement;
   messsageBadge = '';
 
   constructor(
