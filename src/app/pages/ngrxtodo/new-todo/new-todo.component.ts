a74170f23de203c409731c54108903bb2a6987e2
kobepeng <pengkobe123@gmail.com>
2019-06-17 10:12:34 +0800

    feat(todo): support add tag

--- src/app/pages/ngrxtodo/new-todo/new-todo.component.ts
+++ src/app/pages/ngrxtodo/new-todo/new-todo.component.ts
@@ -6,30 +6,34 @@ import { AppState } from './../redux/ngrxtodo.reducer';
 import * as TodoActions from './../redux/todo/todo.actions';
 
 @Component({
-  selector: 'app-new-todo',
-  templateUrl: './new-todo.component.html'
+    selector: 'app-new-todo',
+    templateUrl: './new-todo.component.html',
+    styleUrls: ['./new-todo.component.scss']
 })
 export class NewTodoComponent implements OnInit {
-  val;
-  textField: FormControl;
+    title;
+    textField: FormControl;
+    todoType = {
+        value: '1'
+    };
 
-  constructor(
-    private store: Store<AppState>
-  ) {
-    this.textField = new FormControl('', [Validators.required]);
-  }
-
-  ngOnInit() {
-  }
-
-  saveTodo() {
-    if (this.textField.valid) {
-      const title: string = this.textField.value;
-      const action = new TodoActions.AddTodoAction(title.trim(), 1);
-      this.store.dispatch(action);
-      this.textField.setValue('', { emitEvent: false });
-      this.val = '';
+    constructor(
+        private store: Store<AppState>
+    ) {
+        this.textField = new FormControl('', [Validators.required]);
     }
-  }
+
+    ngOnInit() {
+    }
+
+    saveTodo() {
+        if (this.textField.valid) {
+            const title: string = this.textField.value;
+            const action = new TodoActions.AddTodoAction(title.trim(), parseInt(this.todoType.value, 10));
+            this.store.dispatch(action);
+            this.textField.setValue('', { emitEvent: false });
+            this.title = '';
+        }
+    }
 
 }
